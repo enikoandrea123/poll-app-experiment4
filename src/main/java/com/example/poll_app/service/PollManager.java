15ccd73200779792eaea568cefc805c678227440
Beke Eniko Andrea <enikoandrea12@gmail.com>
2025-08-31 19:32:09 +0200

    Implement HashMaps for polls and users

--- src/main/java/com/example/poll_app/service/PollManager.java
+++ src/main/java/com/example/poll_app/service/PollManager.java
@@ -1,4 +1,15 @@
 package com.example.poll_app.service;
 
+import com.example.poll_app.domain.Poll;
+import com.example.poll_app.domain.User;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class PollManager {
+    private final Map<String, Poll> polls=new HashMap<>();
+    private final Map<String, User> users=new HashMap<>();
+
 }
