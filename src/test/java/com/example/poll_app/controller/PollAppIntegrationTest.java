9e51eaefa4b059c60f2a4850d76bcea84e02b864
Beke Eniko Andrea <enikoandrea12@gmail.com>
2025-09-05 17:07:41 +0200

    Add @DirtiesContext to ensures the database resets after each test

--- src/test/java/com/example/poll_app/controller/PollAppIntegrationTest.java
+++ src/test/java/com/example/poll_app/controller/PollAppIntegrationTest.java
@@ -19,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class PollAppIntegrationTest {
 
     @Autowired
