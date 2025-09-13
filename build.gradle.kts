ec476cba13dd814de5e7e295fb35c308d2f71299
Beke Eniko Andrea <enikoandrea12@gmail.com>
2025-09-13 18:46:24 +0200

    add missing file (that I thought I've had before) to test localhost:8080

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,22 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.5.5"
-	id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.3"
+    java
 }
 
 group = "com.example"
-version = "0.0.1-SNAPSHOT"
-description = "Demo project for Spring Boot"
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
